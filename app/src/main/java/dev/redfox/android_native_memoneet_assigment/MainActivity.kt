51324e62a476deb755f22b881cec582636d09493
madhurmehta007 <12madhurmehta@gmail.com>
2024-03-04 18:35:54 +0530

    Initial commit

---
+++ app/src/main/java/dev/redfox/android_native_memoneet_assigment/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.redfox.android_native_memoneet_assigment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
