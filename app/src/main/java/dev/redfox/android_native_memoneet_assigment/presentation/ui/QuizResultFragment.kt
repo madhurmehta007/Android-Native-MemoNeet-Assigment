10885e58434345f407021fe645c2273ec63c44e5
madhurmehta007 <12madhurmehta@gmail.com>
2024-03-05 13:24:34 +0530

    Made some design changes

--- app/src/main/java/dev/redfox/android_native_memoneet_assigment/presentation/ui/QuizResultFragment.kt
+++ app/src/main/java/dev/redfox/android_native_memoneet_assigment/presentation/ui/QuizResultFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import dev.redfox.android_native_memoneet_assigment.R
 import dev.redfox.android_native_memoneet_assigment.databinding.FragmentQuizResultBinding
 import dev.redfox.android_native_memoneet_assigment.presentation.viewmodel.CapsuleViewModel
@@ -31,16 +32,27 @@ class QuizResultFragment : Fragment() {
 
         capsuleViewModel.userAnswers.observe(viewLifecycleOwner, Observer {
             val map = capsuleViewModel.calculateResults()
-
-            binding.totalNumber.text = "${capsuleViewModel.quizQuestions.size}"
+            var accuracy = ""
+             accuracy ="${
+                map.get(Constants.CORRECT_ANSWER)?.toFloat()
+                    ?.div(map.get(Constants.TOTAL_ANSWERED)!!.toFloat())?.times(100)
+            }"
+                binding.totalNumber.text = "${capsuleViewModel.quizQuestions.size}"
             binding.answeredNumber.text = "${map.get(Constants.TOTAL_ANSWERED)}"
             binding.unansweredNumber.text = "${map.get(Constants.UNANSWERED_COUNT)}"
             binding.correctNumber.text = "${map.get(Constants.CORRECT_ANSWER)}"
             binding.wrongNumber.text = "${map.get(WRONG_ANSWER)}"
-            binding.scoreNumber.text = "${map.get(Constants.CORRECT_ANSWERS_COUNT)}"
+            binding.tvScoreValue.text = "${map.get(Constants.CORRECT_ANSWERS_COUNT)} points"
+            if(accuracy != "NaN"){
+                binding.tvAccuracyValue.text = "${accuracy} %"
+            }else{
+                binding.tvAccuracyValue.text = "0.0 %"
+            }
 
         })
 
-
+        binding.btnEndCapsule.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 }
